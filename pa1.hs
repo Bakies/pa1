9f8f04813845fb4d6cf0a4f711007c43a01af507
Jon Bakies <jon@baki.es>
2016-11-02 01:36:57 -0400

    Added some comments, removed unneccessary code

--- pa1.hs
+++ pa1.hs
@@ -36,37 +36,44 @@ getPriority (_,prio,_) = prio
 -- TODO: implement the functions below
 -- merge two heaps together while maintaining heap properties
 merge :: Heap Patient -> Heap Patient -> Heap Patient
-merge (HeapNode x l r) Tip = HeapNode x l r 
-merge Tip (HeapNode x l r) = HeapNode x l r
-merge (HeapNode x x1 x2)  (HeapNode y y1 y2)
-      | getPriority x > getPriority y =  (HeapNode x (merge (HeapNode y y1 y2)  x2) x1)
-      | getPriority x <= getPriority y =  (HeapNode y (merge (HeapNode x x1 x2) y2) y1)
+-- get nothing give nothing
+merge Tip Tip = Tip
+-- if right heap is empty return left
+merge x Tip = x
+-- vice versa
+merge Tip x = x
+merge (HeapNode lp ll lr)  (HeapNode rp rl rr)
+      | getPriority lp > getPriority rp =  (HeapNode lp (merge (HeapNode rp rl rr)  lr) ll)
+      | getPriority lp <= getPriority rp =  (HeapNode rp (merge (HeapNode lp ll lr) rr) rl)
 
 -- return the patient with the highest priority
-
 peek :: Heap Patient -> Patient
+peek Tip = error "Cant peek empty heap"
 peek (HeapNode p _ _) = p 
 
 -- convert a heap to a list of patients, with the list showing the tree from left to right
 heap2InList :: Heap Patient -> [Patient]
+heap2InList Tip = []
+-- add patient to list if it has no childrn
 heap2InList (HeapNode p Tip Tip) = p:[]
-heap2InList (HeapNode p l Tip) = (heap2InList l) ++  [p]
-heap2InList (HeapNode p Tip r) =  p : (heap2InList r)
+-- take the list from the left node and add it to this patient then add everything on the right
 heap2InList (HeapNode p l r) = (heap2InList l) ++ [p] ++ (heap2InList r)
 
 
 -- convert a heap to a list of patients with each node printed before its children
 heap2PreList :: Heap Patient -> [Patient]
+-- get nothing, give nothing
+heap2PreList Tip = []
+-- if patient has no children prepend it to an empty list
 heap2PreList (HeapNode p Tip Tip) = p:[]
-heap2PreList (HeapNode p l Tip) = [p] ++ (heap2PreList l)
-heap2PreList (HeapNode p Tip r) = [p] ++ (heap2PreList r)
 heap2PreList (HeapNode p l r)   = [p] ++ (heap2PreList l) ++ (heap2PreList r)
 
 -- extract all of the patient names in order of top-to-bottom, left-to-right
 heap2Names :: Heap Patient -> [Name]
+-- get nothing, give nothing
+heap2Names Tip = []
+-- if no children add the name to empty list
 heap2Names (HeapNode (n, _, _) Tip Tip) = n:[]
-heap2Names (HeapNode (n, _, _) l Tip)   = [n] ++ (heap2Names l)
-heap2Names (HeapNode (n, _, _) Tip r)   = [n] ++ (heap2Names r)
 heap2Names (HeapNode (n, _, _) l r)     = [n] ++ (heap2Names l) ++ (heap2Names r)
 
 -- return whether the heap has no patients in it
@@ -87,5 +94,6 @@ heapMap :: (Bill -> Bill) -> Heap Patient -> Heap Patient
 heapMap _ Tip = Tip 
 heapMap func (HeapNode (n, p, b) l r) = (HeapNode (n, p, func b) (heapMap func l) (heapMap func r))
 
+-- just get the size of list of all the patients names
 size :: Heap Patient -> Int
 size x = length (heap2Names x)
